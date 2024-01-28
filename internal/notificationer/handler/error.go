ae5878e90f15ca9783886cb6c69088866829e1b9
Lisandro Torresetti <lisandro.ltorresetti@mercadolibre.com>
2024-01-28 13:32:20 -0300

    WIP Implement: scheduleNotification, getNotifications, getNotificationData and deleteNotification

--- internal/notificationer/handler/error.go
+++ internal/notificationer/handler/error.go
@@ -1,1 +1,49 @@
-package error
+package handler
+
+import (
+	"errors"
+	"net/http"
+)
+
+type ErrorResponse struct {
+	StatusCode int    `json:"status_code"`
+	Message    string `json:"message"`
+}
+
+var (
+	errGettingAppContext             = errors.New("error getting app")
+	errInvalidNotificationBody       = errors.New("error invalid notification request body")
+	errNotificationRequestValidation = errors.New("error notification request validation")
+	errSchedulingNotification        = errors.New("error scheduling notification")
+	errUserNotAllowed                = errors.New("error user not allowed")
+	errFetchingUserNotifications     = errors.New("error fetching notifications")
+	errFetchingNotification          = errors.New("error fetching notification")
+	errMissingNotificationID         = errors.New("error missing notificationID")
+	errDeletingNotification          = errors.New("error deleting notification")
+)
+
+var statusCodeByErr = map[error]int{
+	errGettingAppContext:             http.StatusInternalServerError,
+	errSchedulingNotification:        http.StatusInternalServerError,
+	errFetchingUserNotifications:     http.StatusInternalServerError,
+	errDeletingNotification:          http.StatusInternalServerError,
+	errInvalidNotificationBody:       http.StatusBadRequest,
+	errNotificationRequestValidation: http.StatusBadRequest,
+	errMissingNotificationID:         http.StatusBadRequest,
+	errUserNotAllowed:                http.StatusUnauthorized,
+}
+
+func NerErrorResponse(err error) ErrorResponse {
+	errCode, ok := statusCodeByErr[err]
+	if ok {
+		return ErrorResponse{
+			StatusCode: errCode,
+			Message:    err.Error(),
+		}
+	}
+
+	return ErrorResponse{
+		StatusCode: http.StatusInternalServerError,
+		Message:    err.Error(),
+	}
+}
--- internal/notificationer/handler/handler.go
+++ internal/notificationer/handler/handler.go
@@ -1,8 +1,19 @@
 package handler
 
-import "github.com/gin-gonic/gin"
+import (
+	"fmt"
+	"github.com/gin-gonic/gin"
+	"net/http"
+	"notification-scheduler/internal/domain"
+	"notification-scheduler/internal/internal/context"
+	"notification-scheduler/internal/notificationer/handler/internal/validator"
+)
 
 type servicer interface {
+	ScheduleNotification(notification domain.Notification) error
+	GetNotificationsByUserEmail(email string) ([]domain.Notification, error)
+	GetNotification(notificationID string) (domain.Notification, error)
+	DeleteNotification(notificationID string) error
 }
 
 type NotificationHandler struct {
@@ -16,15 +27,113 @@ func NewNotificationHandler(service servicer) *NotificationHandler {
 }
 
 func (nh *NotificationHandler) ScheduleNotification(c *gin.Context) {
-	panic("implement me!")
+	appContext, err := context.GetAppContext(c)
+	if err != nil {
+		errResponse := NerErrorResponse(fmt.Errorf("%w: %v", errGettingAppContext, err))
+		c.JSON(errResponse.StatusCode, errResponse)
+		return
+	}
+
+	var notificationRequest domain.NotificationRequest
+	err = c.ShouldBindJSON(&notificationRequest)
+	if err != nil {
+		errResponse := NerErrorResponse(fmt.Errorf("%w: %v", errInvalidNotificationBody, err))
+		c.JSON(errResponse.StatusCode, errResponse)
+		return
+	}
+
+	notificationRequest.Via = domain.Telegram
+	if !appContext.TelegramRequest {
+		notificationRequest.UserID = appContext.Email
+		notificationRequest.Via = domain.Mail
+	}
+
+	err = validator.ValidateNotificationRequest(notificationRequest)
+	if err != nil {
+		errResponse := NerErrorResponse(fmt.Errorf("%w: %v", errNotificationRequestValidation, err))
+		c.JSON(errResponse.StatusCode, errResponse)
+		return
+	}
+
+	notification := notificationRequest.ToNotification()
+	err = nh.service.ScheduleNotification(notification)
+	// ToDo: handle idempotent messages
+	if err != nil {
+		errResponse := NerErrorResponse(fmt.Errorf("%w: %v", errSchedulingNotification, err))
+		c.JSON(errResponse.StatusCode, errResponse)
+		return
+	}
+
+	notificationResponse := domain.NewNotificationResponse(notification)
+	c.JSON(http.StatusCreated, notificationResponse)
 }
 
 func (nh *NotificationHandler) GetNotifications(c *gin.Context) {
-	panic("implement me!")
+	appContext, err := context.GetAppContext(c)
+	if err != nil {
+		errResponse := NerErrorResponse(fmt.Errorf("%w: %v", errGettingAppContext, err))
+		c.JSON(errResponse.StatusCode, errResponse)
+		return
+	}
+
+	userIDParam := c.Param("userID")
+	if userIDParam == "" || userIDParam != appContext.UserID {
+		errResponse := NerErrorResponse(fmt.Errorf("%w: userID %s", errUserNotAllowed, appContext.UserID))
+		c.JSON(errResponse.StatusCode, errResponse)
+		return
+	}
+
+	notifications, err := nh.service.GetNotificationsByUserEmail(appContext.Email)
+	if err != nil {
+		errResponse := NerErrorResponse(fmt.Errorf("%w: %v", errFetchingUserNotifications, err))
+		c.JSON(errResponse.StatusCode, errResponse)
+		return
+	}
+
+	if len(notifications) == 0 {
+		c.JSON(http.StatusNoContent, nil)
+		return
+	}
+
+	var response []domain.NotificationResponse
+	for idx := range notifications {
+		response = append(response, domain.NewNotificationResponse(notifications[idx]))
+	}
+
+	c.JSON(http.StatusOK, response)
 }
 
 func (nh *NotificationHandler) GetNotificationData(c *gin.Context) {
-	panic("implement me!")
+	appContext, err := context.GetAppContext(c)
+	if err != nil {
+		errResponse := NerErrorResponse(fmt.Errorf("%w: %v", errGettingAppContext, err))
+		c.JSON(errResponse.StatusCode, errResponse)
+		return
+	}
+
+	notificationID := c.Param("notificationID")
+	if notificationID == "" {
+		errResponse := NerErrorResponse(errMissingNotificationID)
+		c.JSON(errResponse.StatusCode, errResponse)
+		return
+	}
+
+	notification, err := nh.service.GetNotification(notificationID)
+	if err != nil {
+		// ToDo: handle 404
+		errResponse := NerErrorResponse(fmt.Errorf("%w: %v", errFetchingNotification, err))
+		c.JSON(errResponse.StatusCode, errResponse)
+		return
+	}
+
+	if notification.UserID != appContext.Email {
+		errResponse := NerErrorResponse(fmt.Errorf("%w: userID %s", errUserNotAllowed, appContext.UserID))
+		c.JSON(errResponse.StatusCode, errResponse)
+		return
+	}
+
+	response := domain.NewNotificationResponse(notification)
+	c.JSON(http.StatusOK, response)
 }
 
 func (nh *NotificationHandler) UpdateNotification(c *gin.Context) {
@@ -32,5 +141,41 @@ func (nh *NotificationHandler) UpdateNotification(c *gin.Context) {
 }
 
 func (nh *NotificationHandler) DeleteNotification(c *gin.Context) {
-	panic("implement me!")
+	appContext, err := context.GetAppContext(c)
+	if err != nil {
+		errResponse := NerErrorResponse(fmt.Errorf("%w: %v", errGettingAppContext, err))
+		c.JSON(errResponse.StatusCode, errResponse)
+		return
+	}
+
+	notificationID := c.Param("notificationID")
+	if notificationID == "" {
+		errResponse := NerErrorResponse(errMissingNotificationID)
+		c.JSON(errResponse.StatusCode, errResponse)
+		return
+	}
+
+	// Sanity check: only the user that creates the notification can delete it
+	notification, err := nh.service.GetNotification(notificationID)
+	if err != nil {
+		// ToDo: handle 404
+		errResponse := NerErrorResponse(fmt.Errorf("%w: %v", errFetchingNotification, err))
+		c.JSON(errResponse.StatusCode, errResponse)
+		return
+	}
+
+	if notification.UserID != appContext.Email {
+		errResponse := NerErrorResponse(fmt.Errorf("%w: cannot delete notification, userID %s", errUserNotAllowed, appContext.UserID))
+		c.JSON(errResponse.StatusCode, errResponse)
+		return
+	}
+
+	err = nh.service.DeleteNotification(notificationID)
+	if err != nil {
+		errResponse := NerErrorResponse(fmt.Errorf("%w: %v", errDeletingNotification, err))
+		c.JSON(errResponse.StatusCode, errResponse)
+		return
+	}
+
+	c.JSON(http.StatusOK, nil)
 }
