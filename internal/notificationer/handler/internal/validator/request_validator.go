eb9be7cef419c6782c37a014c2b6cb6b8109c4df
Lisandro Torresetti <lisandro.ltorresetti@mercadolibre.com>
2024-01-28 13:31:25 -0300

    Minor changes

---
+++ internal/notificationer/handler/internal/validator/request_validator.go
@@ -0,0 +1,8 @@
+package validator
+
+import "notification-scheduler/internal/domain"
+
+func ValidateNotificationRequest(notification domain.NotificationRequest) error {
+	// ToDo: implement me. Licha
+	return nil
+}
