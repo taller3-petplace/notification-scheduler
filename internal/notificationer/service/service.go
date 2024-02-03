e9502fee63b823820333135f687b942ac5adbfd0
Lisandro Torresetti <ltorresetti@fi.uba.ar>
2024-02-03 17:29:35 -0300

    WIP: service

--- internal/notificationer/service/service.go
+++ internal/notificationer/service/service.go
@@ -4,33 +4,64 @@ import (
 	"notification-scheduler/internal/domain"
 )
 
+type searchFunction func(notification domain.Notification) bool
+
 type database interface {
+	CreateNotifications(notifications []domain.Notification) error
+	GetNotification(notificationID string) (*domain.Notification, error)
+	DeleteNotification(notificationID string) (bool, error)
 }
 
 type NotificationService struct {
 	db database
 }
 
-func NewNotificationService() *NotificationService {
-	return &NotificationService{}
+func NewNotificationService(db database) *NotificationService {
+	return &NotificationService{
+		db: db,
+	}
 }
 
-func (ns *NotificationService) ScheduleNotification(notification domain.Notification) error {
-	//TODO implement me
-	panic("implement me")
+// ScheduleNotifications creates the notifications. From one notification multiple can be created. This method
+// contains all the logic to create the corresponding amount of notifications.
+func (ns *NotificationService) ScheduleNotifications(notification domain.Notification) error {
+
+	return nil
 }
 
+// GetNotificationsByUserEmail searches notifications based on the given search function. This function works as a filter
+// in order to retrieve certain notifications
 func (ns *NotificationService) GetNotificationsByUserEmail(email string) ([]domain.Notification, error) {
 	//TODO implement me
 	panic("implement me")
 }
 
+// GetNotification returns a single notification. If it does not exist, an error is returned
 func (ns *NotificationService) GetNotification(notificationID string) (domain.Notification, error) {
-	//TODO implement me
-	panic("implement me")
+	operation := "GetNotification"
+	notification, err := ns.db.GetNotification(notificationID)
+	if err != nil {
+		return domain.Notification{}, newInternalError(operation, err, "notificationID: "+notificationID)
+	}
+
+	if notification == nil {
+		return domain.Notification{}, newNotificationNotFoundError(operation, "notificationID: "+notificationID)
+	}
+
+	return *notification, err
 }
 
+// DeleteNotification deletes a single notification. If it does not exist, an error is returned
 func (ns *NotificationService) DeleteNotification(notificationID string) error {
-	//TODO implement me
-	panic("implement me")
+	operation := "DeleteNotification"
+	deleted, err := ns.db.DeleteNotification(notificationID)
+	if err != nil {
+		return newInternalError(operation, err, "notificationID: "+notificationID)
+	}
+
+	if !deleted {
+		return newNotificationNotFoundError(operation, "notificationID: "+notificationID)
+	}
+
+	return nil
 }
