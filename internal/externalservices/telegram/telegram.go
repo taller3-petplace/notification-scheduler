fdb4bc5ddf52ee2d0a860e3bff9e4e996998ae80
Lisandro Torresetti <ltorresetti@fi.uba.ar>
2024-02-05 01:03:52 -0300

    create access token

--- internal/externalservices/telegram/telegram.go
+++ internal/externalservices/telegram/telegram.go
@@ -2,14 +2,22 @@ package telegram
 
 import (
 	"fmt"
+	"github.com/golang-jwt/jwt/v5"
 	"github.com/sirupsen/logrus"
 	"io"
 	"net/http"
 	"notification-scheduler/internal/domain"
 	"notification-scheduler/internal/externalservices/telegram/internal/notification"
+	"notification-scheduler/internal/internal/headers"
+	"os"
+	"time"
 )
 
-const url = "https://api.lnt.digital/telegram/notifications"
+const (
+	telegramSecretEnvVar     = "TELEGRAM_SECRET"
+	telegramAccessCodeEnvVar = "TELEGRAM_ACCESS_CODE"
+	url                      = "https://api.lnt.digital/telegram/notifications"
+)
 
 // Telegramer makes requests against Telegram Service
 type Telegramer struct {
@@ -37,6 +45,13 @@ func (t *Telegramer) SendNotifications(notifications []domain.Notification) erro
 		return err
 	}
 
+	accessToken, err := createAccessToken()
+	if err != nil {
+		logrus.Errorf("error creating telegram access token: %v", err)
+		return fmt.Errorf("error creating token: %v", err)
+	}
+
+	request.Header.Add(headers.JWT, accessToken)
 	response, err := t.clientHTTP.Do(request)
 	if err != nil {
 		logrus.Errorf("error performing SendNotifications: %v", err)
@@ -68,4 +83,30 @@ func (t *Telegramer) SendNotifications(notifications []domain.Notification) erro
 	logrus.Infof("Notification sent summary: %s", string(responseBody))
 
 	return nil
+}
+
+// createAccessToken required token to make requests against Telegram Service
+func createAccessToken() (string, error) {
+	accessCode := os.Getenv(telegramAccessCodeEnvVar)
+	if accessCode == "" {
+		return "", fmt.Errorf("error access code is missing")
+	}
+
+	secret := os.Getenv(telegramSecretEnvVar)
+	if secret == "" {
+		return "", fmt.Errorf("error telegram secret is missing")
+	}
+
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256,
+		jwt.MapClaims{
+			"access_code": accessCode,
+			"exp":         time.Now().Add(2 * time.Minute).Unix(),
+		})
+
+	tokenString, err := token.SignedString(secret)
+	if err != nil {
+		return "", err
+	}
+
+	return tokenString, nil
 }
